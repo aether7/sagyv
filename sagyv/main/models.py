7da7c8bee6e3ed5e10f8fcd8fc96d36870ba8596
aether7 <seba.real.a@gmail.com>
2014-04-23 22:55:19 -0300

    se agrega primera version de la DB

--- sagyv/main/models.py
+++ sagyv/main/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoEntrega(models.Model):
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=140)
+    region = models.ForeignKey(Region)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=140)
+    direccion = models.TextField()
+    telefono = models.CharField(max_length=140)
+    rut = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre + " " + self.telefono
+
+
+class Cupon(models.Model):
+    modalidad_reparto = models.ForeignKey(TipoEntrega)
+    razon_social = models.CharField(max_length=140)
+    numero_cupon = models.IntegerField(unique=True)
+    numero_factura = models.IntegerField(unique=True)
+    fecha = models.DateField(auto_now_add=True)
+    lugar_emision = models.CharField(max_length=200)
+    detalle = models.TextField()
+    zonal = models.ForeignKey(Comuna)
+    cantidad_kilos = models.IntegerField()
+    cliente = models.ForeignKey(Cliente)
+
+    def __unicode__(self):
+        return str(self.numero_cupon) + " " + self.zonal.nombre
+
+
+class TipoPago(models.Model):
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class TipoTarjeta(models.Model):
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class BoletaPago(models.Model):
+    tipo_pago = models.ForeignKey(TipoPago)
+    fecha = models.DateTimeField()
+    terminal = models.CharField(max_length=140)
+    fecha_contable = models.DateField()
+    monto = models.IntegerField()
+    numero_operacion = models.IntegerField()
+    codigo_autorizacion = models.IntegerField()
+    numero_tarjeta = models.IntegerField(null=True)
+    tipo_tarjeta = models.ForeignKey(TipoTarjeta, null=True)
+    numero_cuenta = models.IntegerField(null=True)
+    marca = models.CharField(max_length=140,null=True)
+    tipo_cuotas = models.CharField(max_length=140,null=True)
+    numero_cuotas = models.IntegerField(default=1)
+
+    def __unicode__(self):
+        return ""
+
+
+class CuotasBoleta(models.Model):
+    boleta_pago = models.ForeignKey(BoletaPago)
+    monto = models.IntegerField()
+
+    def __unicode(self):
+        return self.monto
