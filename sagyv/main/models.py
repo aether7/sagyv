eebaa30e59223b1993609853a5d51197e3c2c4f2
Norman Glaves <n.glaves@gmail.com>
2014-04-30 16:16:37 -0400

    En proceso

--- sagyv/main/models.py
+++ sagyv/main/models.py
@@ -28,29 +28,30 @@ class Comuna(models.Model):
         return self.nombre
 
 
+class FormatoProducto(models.Model):
+    nombre = models.CharField(max_length=140)
+    peso = IntegerField()
+
+    def __unicode__(self):
+        return self.nombre
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=140)
     direccion = models.TextField()
     telefono = models.CharField(max_length=140)
     rut = models.CharField(max_length=140)
+    razon_social = models.CharField(max_length=140)
 
     def __unicode__(self):
         return self.nombre + " " + self.telefono
 
 
 class Cupon(models.Model):
-    razon_social = models.CharField(max_length=140)
     fecha = models.DateField(auto_now_add=True)
-    formato = models.IntegerField()
     numero_cupon = models.IntegerField(unique=True)
     cliente = models.ForeignKey(Cliente)
-    numero_factura = models.IntegerField(unique=True)
-    #lugar_emision = models.CharField(max_length=200)
-    #detalle = models.TextField()
-    #zonal = models.ForeignKey(Comuna)
-    #cantidad_kilos = models.IntegerField()
-    #modalidad_reparto = models.ForeignKey(TipoEntrega)
-    #trabajador = models.ForeignKey(Trabajador)
+    formato = models.ForeignKey(FormatoProducto)
+    trabajador = models.ForeignKey(Trabajador)
 
     def __unicode__(self):
         return str(self.numero_cupon)
@@ -65,13 +66,14 @@ class TipoPago(models.Model):
 
 class TipoTarjeta(models.Model):
     nombre = models.CharField(max_length=140)
+    marca = models.CharField(max_length=140)
 
     def __unicode__(self):
         return self.nombre
 
 
 class BoletaPago(models.Model):
-    marca = models.CharField(max_length=140,null=True) #se deberia hacer una tabla con marca.(dependiendo de la marca tener el tipo)
+    tipo_tarjeta = models.ForeignKey(TipoTarjeta, null=True)
     tipo_pago = models.ForeignKey(TipoPago)
     tipo_cuotas = models.CharField(max_length=140,null=True)
     fecha = models.DateTimeField()
@@ -81,10 +83,6 @@ class BoletaPago(models.Model):
     codigo_autorizacion = models.IntegerField()
     numero_cuotas = models.IntegerField(default=1)
     monto = models.IntegerField()
-    
-    #fecha_contable = models.DateField()
-    #tipo_tarjeta = models.ForeignKey(TipoTarjeta, null=True) -> me surge la duda por que existe este...
-    #numero_cuenta = models.IntegerField(null=True)
 
     def __unicode__(self):
         return ""
@@ -94,12 +92,63 @@ class CuotasBoleta(models.Model):
     boleta_pago = models.ForeignKey(BoletaPago)
     monto = models.IntegerField()
 
-    def __unicode(self):
+    def __unicode__(self):
         return self.monto
 
+
 class CierreRepartidor(models.Model):
-    chofer = models.ForeignKey(Trabajador)
-    fecha = models.DateTimeField()
-    oficina = models.IntegerField()
-    terminal = models.IntegerField()
     numero_cierre = models.IntegerField()
+    fecha = models.DateTimeField()
+    correlativo_cierre = models.IntegerField()
+    chofer = models.ForeignKey(Trabajador)
+    total_descuentos = IntegerField()
+
+
+class TargetaComercial(models.Model):
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class TipoDescuento(models.Model):
+    nombre = models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class DetalleTargetaCierreRepartidor(models.Model):
+    cierre = models.ForeignKey(CierreRepartidor)
+    targeta_comercial = models.ForeignKey(TargetaComercial)
+    monto_total = models.IntegerField()
+
+    def __unicode__(self):
+        return ""
+
+
+class DetalleDescuentoCierreRepartidor(models.Model):
+    cierre = models.ForeignKey(CierreRepartidor)
+    tipo_descuento = models.ForeignKey(TipoDescuento)
+    monto_total = models.IntegerField()
+
+    def __unicode__(self):
+        return ""
+
+
+class DescuentoCliente(models.Model):
+    nombre = models.ForeignKey(Cliente)
+    
+
+
+class GuiaDespasho(models.Model):
+    cliente = models.ForeignKey(Cliente)
+    chofer = models.ForeignKey(Trabajador)
+    numero = models.IntegerField()
+    fecha = models.DateField(auto_now_add=True)
+    sub_total = models.IntegerField()
+    descuento_cliente = models.ForeignKey()
+
+    def __unicode__(self):
+        return self.numero
+
