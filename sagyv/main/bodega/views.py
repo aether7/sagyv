93d0c4a81c08e9a47bca34b649e84e00ad0199af
Norman Th. Glaves <n.glaves@gmail.com>
2014-06-07 17:27:00 -0400

    Primer apronte de bodega

--- sagyv/main/bodega/views.py
+++ sagyv/main/bodega/views.py
@@ -1,3 +1,12 @@
 from django.views.generic import TemplateView,View
+from main.models import Producto
 
-index = TemplateView.as_view(template_name="bodega/index.html")
+class IndexView(TemplateView):
+	template_name = "bodega/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context["productos"] = Producto.objects.all()
+		return context
+
+index = IndexView.as_view()
