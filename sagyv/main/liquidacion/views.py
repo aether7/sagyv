af4235f4666e709f6e7019cd58911b22917a30c2
Sebastian Real <seba.real.a@gmail.com>
2014-06-07 17:37:49 -0400

    se agrega motor de sugerencias

--- sagyv/main/liquidacion/views.py
+++ sagyv/main/liquidacion/views.py
@@ -1,3 +1,4 @@
+import json
 from django.views.generic import TemplateView,View
 from main.models import Trabajador
 
@@ -6,7 +7,15 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context["trabajadores"] = Trabajador.objects.all()
+        context["trabajadores"] = []
+
+        for trabajador in Trabajador.objects.all():
+            context["trabajadores"].append(json.dumps({
+                "nombre" : trabajador.nombre,
+                "apellido" : trabajador.apellido,
+                "id" : trabajador.id,
+                "nombre_completo" : trabajador.get_nombre_completo()
+            }))
 
         return context
 
---
+++ sagyv/static/js/controllers/liquidacion.js
@@ -0,0 +1,50 @@
+App.Controllers.Liquidacion = function(){
+    this.empleados = [];
+    this.empleadoElegido = null;
+};
+
+App.Controllers.Liquidacion.prototype = {
+    constructor: App.Controllers.Liquidacion,
+    init: function(){
+        var _this = this,
+            empleadosSuggest;
+
+        empleadosSuggest = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace("nombre_completo"),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: $.map(this.empleados,function(empleado){ return empleado; })
+        });
+
+        empleadosSuggest.initialize();
+
+        $("#busca_trabajador").typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 3
+        },{
+            name: 'nombreEmpleados',
+            displayKey: 'nombre_completo',
+            source: empleadosSuggest.ttAdapter()
+        });
+
+        $("#busca_trabajador").on("typeahead:selected",function(){
+            _this.seleccionarTrabajador($(this).val());
+        });
+    },
+
+    addTrabajador: function(trabajador){
+        var trabajadorJson = JSON.parse(trabajador.replace(/&quot;/g, '"'));
+        this.empleados.push(trabajadorJson);
+    },
+
+    seleccionarTrabajador: function(nombreTrabajador){
+        var _this = this;
+
+        this.empleados.forEach(function(trabajador){
+            if(trabajador.nombre_completo === nombreTrabajador){
+                _this.empleadoElegido = trabajador;
+                $("#guia_despacho").val(_.random(1000,3500));
+            }
+        });
+    }
+};
