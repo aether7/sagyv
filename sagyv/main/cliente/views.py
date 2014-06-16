23c43ffefbb2a1512449921ae330e3e0077f0e3f
Norman Th. Glaves <n.glaves@gmail.com>
2014-06-16 12:52:16 -0400

    Working

--- sagyv/main/cliente/views.py
+++ sagyv/main/cliente/views.py
@@ -1,10 +1,21 @@
 from django.views.generic import TemplateView,View
+from main.models import Cliente
 
 class IndexView(TemplateView):
 	template_name = "cliente/index.html"
 
 	def get_context_data(self, **kwargs):
-		return ""
+		context = super(IndexView, self).get_context_data(**kwargs)
+		context["clientes"] = Cliente.objects.all()
+		return context
 
+
+class CrearClienteView(View):
+	def post(self, request):
+		pass
+
+class CrearDescuentoView(View):
+	def post(self, request):
+		pass
 
 index = IndexView.as_view()
