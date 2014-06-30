275cc8348c55411a56c7a5ffe3e5e593473383e2
Norman Th. Glaves <n.glaves@gmail.com>
2014-06-30 18:43:00 -0400

    cambios

--- sagyv/main/vehiculos/views.py
+++ sagyv/main/vehiculos/views.py
@@ -1,3 +1,14 @@
 from django.views.generic import View, TemplateView
+from main.models import Vehiculo
 
-index = TemplateView.as_view(template_name = "vehiculos/index.html")
+class IndexView(TemplateView):
+    template_name = "vehiculos/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['vehiculos'] = Vehiculo.objects.all()
+        return context
+        
+
+
+index = IndexView.as_view()
