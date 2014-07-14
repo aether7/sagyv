f99c913998dce37b4f64ee44b38da2fbaa319886
Norman Th. Glaves <n.glaves@gmail.com>
2014-07-14 16:36:58 -0400

    primeros pasos Cliente

--- sagyv/main/cliente/urls.py
+++ sagyv/main/cliente/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns("main.cliente.views",
 	url(r"^$", "index", name="cliente"),
+    url(r"^cliente_existe/$", "cliente_existe", name="cliente_existe"),
+    url(r"^crear_cliente/$", "crear_cliente", name="crear_cliente"),
+    url(r"^modificar_cliente/$", "modificar_cliente", name="modificar_cliente"),
+    url(r"^crear_situacion_comercial/$", "crear_situacion_comercial",name="crear_situacion_comercial"),
 )
--- sagyv/main/cliente/views.py
+++ sagyv/main/cliente/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView,View
+from django.http import HttpResponse
 from main.models import Cliente
 
 class IndexView(TemplateView):
@@ -9,13 +10,65 @@ class IndexView(TemplateView):
 		context["clientes"] = Cliente.objects.all()
 		return context
 
+class ClienteExisteView(View):
+
+    def post(self, req):
+        rut = req.POST.get('rut')
+
+        try:
+            cliente_existe = Cliente.objects.get(rut = rut)
+            dato = { "status": "error", "status_message": "El cliente ya existe." }
+        except cliente_existe.DoesNotExist:
+            dato = { "status": "ok" }
+
+        return HttpResponse(json.dumps(dato), content_type="application/json");
+
 
 class CrearClienteView(View):
-	def post(self, request):
+	
+    def post(self, req):
+        giro = req.POST.get('')
+        direccion = req.POST.get('')
+        telefono = req.POST.get('')
+        rut = req.POST.get('')
+        situacion_comercial = req.POST.get('')
+        credito = req.POST.get('')
+
+        try:
+            cliente_existe = Cliente.objects.get(rut = rut)
+            
+        
+        except cliente_existe.DoesNotExist:
+            cliente = Cliente()
+            cliente.giro = giro
+            cliente.direccion = direccion
+            cliente.telefono = telefono
+            cliente.rut = rut
+            cliente.situacion_comercial = situacion_comercial
+            cliente.credito = credito
+            cliente.save()
+        
+        
+
+        
+
+
+class ModificarClienteView(View):
+    
+    def post(self,req):
+        pass
+
+
+class CrearSituacionComercialView(View):
+	
+    def post(self, req):
 		pass
 
-class CrearDescuentoView(View):
-	def post(self, request):
-		pass
 
 index = IndexView.as_view()
+
+cliente_existe = ClienteExisteView.as_view()
+crear_cliente = CrearClienteView.as_view()
+modificar_cliente = ModificarClienteView.as_view()
+
+crear_situacion_comercial = CrearSituacionComercialView.as_view()
