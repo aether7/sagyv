26614b21b8ca02166a7cea02f748148b3a5556e5
Sebastian Real <seba.real.a@gmail.com>
2014-07-14 18:28:53 -0400

    se corrige cliente views, revisar post

--- sagyv/main/cliente/urls.py
+++ sagyv/main/cliente/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns("main.cliente.views",
-	url(r"^$", "index", name="cliente"),
-    url(r'^obtener_cliente/(?P<id_cliente>\d+)/$', "obtener_cliente", name="obtener_cliente"),
-    url(r"^crear_cliente/$", "crear_cliente", name="crear_cliente"),
-    url(r"^modificar_cliente/$", "modificar_cliente", name="modificar_cliente"),
-    url(r"^crear_situacion_comercial/$", "crear_situacion_comercial",name="crear_situacion_comercial"),
+	url(r"^$", "index", name="index"),
+    url(r'^obtener/(?P<id_cliente>\d+)/$', "obtener_cliente", name="obtener"),
+    url(r"^crear/$", "crear_cliente", name="crear"),
+    url(r"^modificar/$", "modificar_cliente", name="modificar"),
+    url(r"^crear\-situacion\-comercial/$", "crear_situacion_comercial",name="crear_situacion_comercial"),
 )
