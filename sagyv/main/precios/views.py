3cd6aecc4abe9baea3cfca4b2947e02e7dce2063
Norman Th. Glaves <n.glaves@gmail.com>
2014-07-22 18:57:24 -0400

    Adaptacion

--- sagyv/main/precios/views.py
+++ sagyv/main/precios/views.py
@@ -4,10 +4,13 @@ from django.db import transaction
 from django.views.generic import View, TemplateView, ListView
 from main.models import Producto
 
-class IndexList(ListView):
-    model = Producto
-    queryset = Producto.objects.exclude(orden=-1).order_by("orden")
-    context_object_name = "productos"
+class IndexView(TemplateView):
     template_name = "precios/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['lista_precios'] = Producto.objects.exclude(tipo_producto_id = 3).order_by('orden')
+        context['lista_precios_garantias'] = Producto.objects.filter(tipo_producto_id = 3)
+        return context
 
-index = IndexList.as_view()
+index = IndexView.as_view()
