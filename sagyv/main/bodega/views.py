9cdf34fc025261b698f76ad74f557c678bb0ee72
Norman Th. Glaves <n.glaves@gmail.com>
2014-07-28 18:23:14 -0400

    Desarrollo : Se inicia implementacion para crea guia de despacho, se crea una funcionalidad unica para crear guias de despacho de entrada y salida.
    desde el frontend los datos necesarios seran
    lista_producto : Lista compuesta {{id_producto:xxx, cantidad:xxx}...}
    numero_guia : numero de guia
    id_vehiculo : id del vehiculo, puede ir en blanco si existe factura
    num_factura : numero de factura, puede ir en blanco si existe id_vehiculo
    fecha_creacion : tengo mis dudas con respecto a esto, por el tema de ingreso mas que nada.

--- sagyv/main/bodega/views.py
+++ sagyv/main/bodega/views.py
@@ -1,8 +1,10 @@
 import json
 from django.views.generic import TemplateView,View
+from django.db import transaction
 from django.http import HttpResponse
 from main.models import Producto, TipoCambioStock, HistorialStock
 from main.models import PrecioProducto, StockVehiculo, Vehiculo
+from main.models import GuiaDespacho
 
 class IndexView(TemplateView):
     template_name = "bodega/index.html"
@@ -58,6 +60,80 @@ class UpdateStockProductoView(View):
         producto.save()
 
         return HttpResponse(new_stock)
+
+class CrearGuiaDespachoView(View):
+
+    @transaction.commit_on_success
+    def post(self,req):
+        #{{id_producto:xxx, cantidad:xxx}...}
+        lista_producto = req.POST.get("lista_producto") 
+        self.numero_guia = req.POST.get("numero_guia")
+        self.id_vehiculo = req.POST.get("id_vehiculo")
+        self.num_factura = req.POST.get("num_factura")
+        #es probable que no se utilice.
+        self.fecha_creacion = req.POST.get("fecha_creacion")
+
+        lista = json.loads(lista_producto)
+        guia = self.crear_guia_despacho()
+
+        if(self.id_vehiculo != ""):
+            self.carga_datos_salida(guia,lista)
+
+        if(self.factura != ""):
+            self.carga_datos_ingreso(guia,lista)
+
+    def crear_guia_despacho(self):
+        guia_despacho = GuiaDespacho()
+        #guia_despacho.fecha = CAMPO AUTO
+        guia_despacho.numero = self.numero_guia
+        
+        if self.id_vehiculo == "":
+            guia_despacho.factura = self.factura
+            guia_despacho.tipo_guia = True
+            guia_despacho.save()
+        elif self.factura == "":
+            movil = Vehiculo.objects.get(pk = self.id_vehiculo)
+            guia_despacho.vehiculo = movil
+            guia_despacho.tipo_guia = False
+            guia_despacho.save()
+        else:
+            guia_despacho = None
+        
+        return guia_despacho
+
+    def carga_datos_ingreso(self, guia, lista):
+        
+        for item in lista:
+            cantidad = int(item["cantidad"])
+            producto = Producto.objects.get(pk = item["id_producto"])
+            producto.stock += cantidad
+            producto.save()
+
+            self.crear_historico(producto, cantidad, guia.numero ,False)
+
+        return "OK"
+
+
+    def carga_datos_salida(self, guia, lista):
+        
+        for item in lista:
+            cantidad = int(item["cantidad"])
+            producto = Producto.objects.get(pk = item["id_producto"])
+            producto.stock -= cantidad
+            producto.save()
+
+            self.crear_historico(producto, cantidad, guia.numero ,True)
+
+        return "OK"
+
+    def crear_historico(self, producto, cantidad, guia_numero,tipo_operacion):
+        historico = HistorialStock()
+        historico.producto = producto
+        historico.cantidad = cantidad
+        #historico.fecha = AUTO DATE
+        historico.tipo_operacion = tipo_operacion
+        historico.guia_despacho = guia_numero
+        historico.save()
 
 
 index = IndexView.as_view()
