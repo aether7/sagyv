08906aa53da8bd45721e8cc10a1348695025dc42
Norman Th. Glaves <n.glaves@gmail.com>
2014-08-07 22:26:24 -0400

    Dummmy

--- sagyv/main/liquidacion/views.py
+++ sagyv/main/liquidacion/views.py
@@ -1,7 +1,7 @@
 import json
 from django.http import HttpResponse
 from django.views.generic import TemplateView,View
-from main.models import Trabajador, Producto
+from main.models import Trabajador, Producto, GuiaDespacho
 
 class IndexView(TemplateView):
     template_name = "liquidacion/index.html"
@@ -37,6 +37,30 @@ class BalanceLiquidacionView(View):
             valor_total += valor_tmp
 
         dato = { 'valor': valor_total }
+
+        return HttpResponse(json.dumps(dato), content_type="application/json")
+
+
+class ObtenerGuiaDespacho(View):
+    def get(self, req):
+        numero_guia = req.GET.get('numero_guia')
+
+        guia = GuiaDespacho.objects.get(numero = numero_guia)
+
+        dato = [
+            {
+                'id': '1',
+                'codigo': '1105',
+                'cantidad': '10',
+                'precio':'5000'
+            },
+            {
+                'id': '2',
+                'codigo': '1115',
+                'cantidad': '5',
+                'precio':'9500'
+            },
+        ]
 
         return HttpResponse(json.dumps(dato), content_type="application/json")
 
