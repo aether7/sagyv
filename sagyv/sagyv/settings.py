e848112d41980249789ba8d7456fc0d7e9be50c5
Sebastian Real <seba.real.a@gmail.com>
2014-08-13 08:55:39 -0400

    cambios a settings y CSS

--- sagyv/sagyv/settings.py
+++ sagyv/sagyv/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'es-CL'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
