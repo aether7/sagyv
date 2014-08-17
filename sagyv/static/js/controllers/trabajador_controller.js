91340957ceb0114a93879fa879f3c5a8b5a52d8c
Sebastian Real <seba.real.a@gmail.com>
2014-08-17 16:21:37 -0400

    actualizado código de trabajador en js
    Se utiliza angularJS para el proyecto

--- sagyv/static/js/controllers/trabajador_controller.js
+++ sagyv/static/js/controllers/trabajador_controller.js
@@ -24,17 +24,19 @@ TrabajadorController.prototype = {
         var action = App.urls.get("trabajador:crear"),
             json = this.trabajador.getJSON();
 
-        $.post(action, json, function(data){
-            var html,
-                tpl = $("#tpl_nuevo_trabajador").html(),
-                fx = Handlebars.compile(tpl);
+        $.post(action, json, this.renderNuevoTrabajador);
+    },
 
-            html = fx(data);
-            $("#lista_trabajadores tbody").append(html);
+    renderNuevoTrabajador: function(data){
+        var html,
+            tpl = $("#tpl_nuevo_trabajador").html(),
+            fx = Handlebars.compile(tpl);
 
-            $("#modal_nuevo").modal("hide");
-            common.agregarMensaje("El trabajador ha sido creado exitosamente");
-        });
+        html = fx(data);
+        $("#lista_trabajadores tbody").append(html);
+
+        $("#modal_nuevo").modal("hide");
+        common.agregarMensaje("El trabajador ha sido creado exitosamente");
     },
 
     verTrabajador: function(id){
@@ -58,11 +60,12 @@ TrabajadorController.prototype = {
         this.http.get(action).success(function(data){
             common.mostrarModal("editar");
             _this.procesarTrabajador(data, "id");
+            _this.trabajador.id = id;
         });
     },
 
     actualizarTrabajador: function(){
-        if(!trabajador.esValido()){
+        if(!this.trabajador.esValido()){
             return;
         }
 
@@ -70,22 +73,50 @@ TrabajadorController.prototype = {
             json = this.trabajador.getJSON(),
             _this = this;
 
+        $.post(action, json, this.renderActualizarTrabajador);
+    },
+
+    renderActualizarTrabajador: function(data){
+        var tr = $("#lista_trabajadores tr[data-id={0}]".format(data.id));
+
+        tr.find("td[data-campo=nombre]").text(data.nombre);
+        tr.find("td[data-campo=apellido]").text(data.apellido);
+        tr.find("td[data-campo=rut]").text(data.rut);
+        tr.find("td[data-campo=estado_vacaciones]").text(data.estado_vacaciones);
+
+        $("#modal_editar").modal("hide");
+        common.agregarMensaje("El trabajador ha sido editado exitosamente");
+    },
+
+    eliminarTrabajador: function(id){
+        if(!confirm("¿Esta seguro(a) de realizar esta acción ?")){
+            return;
+        }
+
+        var action = App.urls.get("trabajador:eliminar"),
+            json = { id : id };
+
         $.post(action, json, function(data){
-            common.agregarMensaje("El trabajador ha sido editado exitosamente");
+            $("#lista_trabajadores tr[data-id={0}]".format(id)).remove();
+            common.agregarMensaje("El trabajador se ha eliminado exitosamente");
         });
     },
 
     procesarTrabajador: function(data, campo){
         campo = campo || "nombre";
 
+        var fechaNac = new Date(common.fecha.agregarCeros(data.nacimiento) + " 00:00:00"),
+            fechaInicio = new Date(common.fecha.agregarCeros(data.fecha_inicio_contrato) + " 00:00:00"),
+            fechaVigencia = new Date(common.fecha.agregarCeros(data.vigencia_licencia) + " 00:00:00");
+
         this.trabajador = new App.Models.Trabajador();
         this.trabajador.nombre = data.nombre;
         this.trabajador.apellido = data.apellido;
         this.trabajador.rut = data.rut;
         this.trabajador.domicilio = data.domicilio;
-        this.trabajador.fechaNacimiento = data.nacimiento;
-        this.trabajador.inicioContrato = data.fecha_inicio_contrato;
-        this.trabajador.vigenciaLicencia = data.vigencia_licencia;
+        this.trabajador.fechaNacimiento = fechaNac;
+        this.trabajador.inicioContrato = fechaInicio;
+        this.trabajador.vigenciaLicencia = fechaVigencia;
         this.trabajador.afp = data.afp[campo];
         this.trabajador.sistemaSalud = data.sistema_salud[campo];
         this.trabajador.estadoCivil = data.estado_civil[campo];
