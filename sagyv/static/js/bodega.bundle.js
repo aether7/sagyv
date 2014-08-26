816b49ee46e73bb698b0a375b1d188bb5e8406f4
Norman Th. Glaves <n.glaves@gmail.com>
2014-08-25 23:20:11 -0400

    Problemas con Angular, no logro enlasar las cosas

--- sagyv/static/js/bodega.bundle.js
+++ sagyv/static/js/bodega.bundle.js
@@ -294,12 +294,21 @@ module.exports = GuiaProductoController;
 function RecargaController($http){
     this.http = $http;
     this.recarga = null;
+    this.producto = {};
 }
 
 RecargaController.mixin({
     iniciarRecarga: function(){
         this.recarga = new App.Models.Recarga();
         $("#modal_recargar_guia").modal("show");
+    },
+
+    agregarRecarga: function(){
+        if(this.producto.id && this.producto.cantidad){
+            this.producto.codigo = $("#" + idSelect + " option:selected").text();
+        }
+
+        console.log(this.producto);
     }
 });
 
