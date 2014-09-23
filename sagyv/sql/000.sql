37e9b2d446186b41fc865a1e28b2c6bee446e69c
Norman Th. Glaves <n.glaves@gmail.com>
2014-09-23 16:24:51 -0300

    Se anexa parche de bancos

--- sagyv/sql/000.sql
+++ sagyv/sql/000.sql
@@ -459,21 +459,3 @@ INSERT INTO main_comuna (id, region_id, nombre) VALUES (15101, 15, 'Arica ');
 INSERT INTO main_comuna (id, region_id, nombre) VALUES (15102, 15, 'Camarones');
 INSERT INTO main_comuna (id, region_id, nombre) VALUES (15201, 15, 'Putre');
 INSERT INTO main_comuna (id, region_id, nombre) VALUES (15202, 15, 'General Lagos ');
-
---Bancos
-INSERT INTO main_banco(id, nombre) VALUES(1, 'Banco Crédito e Inversiones BCI');
-INSERT INTO main_banco(id, nombre) VALUES(2, 'Banco de Chile');
-INSERT INTO main_banco(id, nombre) VALUES(3, 'Banco del Desarrollo');
-INSERT INTO main_banco(id, nombre) VALUES(4, 'Banco Estado');
-INSERT INTO main_banco(id, nombre) VALUES(5, 'Banco Falabella');
-INSERT INTO main_banco(id, nombre) VALUES(6, 'Banco Itaú');
-INSERT INTO main_banco(id, nombre) VALUES(7, 'Banco Internacional');
-INSERT INTO main_banco(id, nombre) VALUES(8, 'Banco Ripley');
-INSERT INTO main_banco(id, nombre) VALUES(9, 'Banco Santander Santiago');
-INSERT INTO main_banco(id, nombre) VALUES(10, 'Banco Security');
-INSERT INTO main_banco(id, nombre) VALUES(11, 'Bice');
-INSERT INTO main_banco(id, nombre) VALUES(12, 'Consorcio');
-INSERT INTO main_banco(id, nombre) VALUES(13, 'Corp Banca');
-INSERT INTO main_banco(id, nombre) VALUES(14, 'Rabobank');
-INSERT INTO main_banco(id, nombre) VALUES(15, 'Scotiabank');
-INSERT INTO main_banco(id, nombre) VALUES(16, 'HSBC Bank');
