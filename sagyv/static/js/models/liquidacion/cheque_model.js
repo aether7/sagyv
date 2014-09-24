c8a47d29231e282aa950f3839dd6e4f39aab7489
Norman Th. Glaves <n.glaves@gmail.com>
2014-09-24 19:45:53 -0300

    Modelos

---
+++ sagyv/static/js/models/liquidacion/cheque_model.js
@@ -0,0 +1,12 @@
+App.Models.Cheque = function(){
+    this.banco = null;
+    this.numero = null;
+    this.monto = null;
+    this.fecha = null;
+
+    this.cheques = [];
+};
+
+App.Models.Cheque.mixin({
+
+});
