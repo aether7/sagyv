c7fd79315ae02e12597dfe1c8dd67942b233eccc
Sebastian Real <seba.real.a@gmail.com>
2014-12-22 17:15:04 -0300

    se agregan pruebas para terminal refactorizado

--- sagyv/main/pruebas/testterminal.py
+++ sagyv/main/pruebas/testterminal.py
@@ -1,7 +1,8 @@
 import json
 
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.core.urlresolvers import reverse
+
 from main.models import Terminal
 from main.models import EstadoTerminal
 
@@ -9,22 +10,20 @@ class TerminalTestCase(TestCase):
     fixtures = ['terminales.json']
 
     def test_todos_terminales(self):
-        cliente = Client()
-        response = cliente.get(reverse('guias:obtener_terminales'))
+        response = self.client.get(reverse('guias:obtener_terminales'))
         data = json.loads(response.content)
 
-        self.assertEqual(response.status_code, 300)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(data.get('terminales')), 1)
 
     def test_crear_terminal_con_movil(self):
-        cliente = Client()
         post_data = {
             "codigo": "rvecter123",
             "movil": "{ \"id\": 1 }"
         }
 
-        response = cliente.post(reverse("guias:crear_terminal"), post_data)
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post(reverse("guias:crear_terminal"), post_data)
+        self.assertEqual(response.status_code, 200, "Error al crear terminal con movil")
 
 
 # assertEqual(a, b)   a == b
