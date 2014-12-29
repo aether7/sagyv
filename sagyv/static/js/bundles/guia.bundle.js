eac1e0688141f0e89ce36a51999ae539464012f2
Norman Th. Glaves <n.glaves@gmail.com>
2014-12-29 10:45:28 -0300

    REBASEEEEEEEEgit status!

--- sagyv/static/js/bundles/guia.bundle.js
+++ sagyv/static/js/bundles/guia.bundle.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/Users/Aether/Proyectos/sagyv/sagyv/static/js/bundles/guia_bundle.js":[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/home/worker8/proyectos/sagyv/sagyv/static/js/bundles/guia_bundle.js":[function(require,module,exports){
 (function(){
 'use strict';
 
@@ -20,7 +20,7 @@ app.controller('BoletaController', ['boletaService', BoletaController]);
 
 })();
 
-},{"../controllers/guias/boleta_controller.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/controllers/guias/boleta_controller.js","../controllers/guias/guia_controller.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/controllers/guias/guia_controller.js","../controllers/guias/terminal_controller.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/controllers/guias/terminal_controller.js","../services/boleta_service.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/boleta_service.js","../services/guia_service.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/guia_service.js","../services/terminal_service.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/terminal_service.js"}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/controllers/guias/boleta_controller.js":[function(require,module,exports){
+},{"../controllers/guias/boleta_controller.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/controllers/guias/boleta_controller.js","../controllers/guias/guia_controller.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/controllers/guias/guia_controller.js","../controllers/guias/terminal_controller.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/controllers/guias/terminal_controller.js","../services/boleta_service.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/services/boleta_service.js","../services/guia_service.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/services/guia_service.js","../services/terminal_service.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/services/terminal_service.js"}],"/home/worker8/proyectos/sagyv/sagyv/static/js/controllers/guias/boleta_controller.js":[function(require,module,exports){
 var Boleta = require('../../models/guias/boleta_model.js');
 
 function BoletaController(service){
@@ -133,7 +133,7 @@ BoletaController.mixin({
 
 module.exports = BoletaController;
 
-},{"../../models/guias/boleta_model.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/models/guias/boleta_model.js"}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/controllers/guias/guia_controller.js":[function(require,module,exports){
+},{"../../models/guias/boleta_model.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/models/guias/boleta_model.js"}],"/home/worker8/proyectos/sagyv/sagyv/static/js/controllers/guias/guia_controller.js":[function(require,module,exports){
 var Guia = require('../../models/guias/guia_model.js');
 
 function GuiaController(){
@@ -148,7 +148,7 @@ GuiaController.mixin({
 
 module.exports = GuiaController;
 
-},{"../../models/guias/guia_model.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/models/guias/guia_model.js"}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/controllers/guias/terminal_controller.js":[function(require,module,exports){
+},{"../../models/guias/guia_model.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/models/guias/guia_model.js"}],"/home/worker8/proyectos/sagyv/sagyv/static/js/controllers/guias/terminal_controller.js":[function(require,module,exports){
 var Terminal = require('../../models/guias/terminal_model.js');
 
 function TerminalController(service){
@@ -281,7 +281,7 @@ TerminalController.mixin({
 
 module.exports = TerminalController;
 
-},{"../../models/guias/terminal_model.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/models/guias/terminal_model.js"}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/models/guias/boleta_model.js":[function(require,module,exports){
+},{"../../models/guias/terminal_model.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/models/guias/terminal_model.js"}],"/home/worker8/proyectos/sagyv/sagyv/static/js/models/guias/boleta_model.js":[function(require,module,exports){
 function Boleta(){
     this.id = null;
     this.inicial = 0;
@@ -366,7 +366,7 @@ Boleta.prototype = {
 
 module.exports = Boleta;
 
-},{}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/models/guias/guia_model.js":[function(require,module,exports){
+},{}],"/home/worker8/proyectos/sagyv/sagyv/static/js/models/guias/guia_model.js":[function(require,module,exports){
 function Guia(){
     this.id = null;
     this.inicial = 0;
@@ -450,7 +450,7 @@ Guia.prototype = {
 
 module.exports = Guia;
 
-},{}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/models/guias/terminal_model.js":[function(require,module,exports){
+},{}],"/home/worker8/proyectos/sagyv/sagyv/static/js/models/guias/terminal_model.js":[function(require,module,exports){
 function Terminal(){
     this.id = null;
     this.codigo = null;
@@ -520,7 +520,7 @@ Terminal.mixin({
 
 module.exports = Terminal;
 
-},{}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/boleta_service.js":[function(require,module,exports){
+},{}],"/home/worker8/proyectos/sagyv/sagyv/static/js/services/boleta_service.js":[function(require,module,exports){
 var serviceUtil = require('./service_util.js');
 
 function boletaService($http){
@@ -557,7 +557,7 @@ function boletaService($http){
 
 module.exports = boletaService;
 
-},{"./service_util.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/service_util.js"}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/guia_service.js":[function(require,module,exports){
+},{"./service_util.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/services/service_util.js"}],"/home/worker8/proyectos/sagyv/sagyv/static/js/services/guia_service.js":[function(require,module,exports){
 var serviceUtil = require('./service_util.js');
 
 function guiaService($http){
@@ -575,7 +575,7 @@ function guiaService($http){
 
 module.exports = guiaService;
 
-},{"./service_util.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/service_util.js"}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/service_util.js":[function(require,module,exports){
+},{"./service_util.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/services/service_util.js"}],"/home/worker8/proyectos/sagyv/sagyv/static/js/services/service_util.js":[function(require,module,exports){
 function noop(){}
 
 function standardError(data){
@@ -637,7 +637,7 @@ exports.postMaker = function($http){
     };
 };
 
-},{}],"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/terminal_service.js":[function(require,module,exports){
+},{}],"/home/worker8/proyectos/sagyv/sagyv/static/js/services/terminal_service.js":[function(require,module,exports){
 var serviceUtil = require('./service_util.js');
 
 function terminalService($http){
@@ -689,4 +689,4 @@ function terminalService($http){
 
 module.exports = terminalService;
 
-},{"./service_util.js":"/Users/Aether/Proyectos/sagyv/sagyv/static/js/services/service_util.js"}]},{},["/Users/Aether/Proyectos/sagyv/sagyv/static/js/bundles/guia_bundle.js"]);
+},{"./service_util.js":"/home/worker8/proyectos/sagyv/sagyv/static/js/services/service_util.js"}]},{},["/home/worker8/proyectos/sagyv/sagyv/static/js/bundles/guia_bundle.js"]);
