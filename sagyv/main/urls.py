fb2beac9bccf372220d7023f61a029a07db0e4f0
Sebastian Real <seba.real.a@gmail.com>
2014-12-29 22:37:59 -0300

    se elimina codigo de main cliente

--- sagyv/main/urls.py
+++ sagyv/main/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, url, include
 
-urlpatterns = patterns("main.views",
-    url(r"^$","panel_control",name="panel_control"),
+urlpatterns = patterns('main.views',
+    url(r'^$','panel_control',name='panel_control'),
 )
 
 urlpatterns += patterns("",
+    url(r'^trabajador/',include('main.trabajador.urls',namespace='trabajador')),
+
     url(r"^liquidacion/",include("main.liquidacion.urls",namespace="liquidacion")),
     url(r"^bodega/",include("main.bodega.urls",namespace = "bodega")),
     url(r"^vehiculos/",include("main.vehiculos.urls",namespace="vehiculos")),
     url(r"^precios/",include("main.precios.urls",namespace="precios")),
-    url(r"^trabajador/",include("main.trabajador.urls",namespace="trabajador")),
     url(r"^guias/", include("main.guias.urls", namespace="guias")),
 )
